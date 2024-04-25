3c51cc45ae955f2b902bf34852e11287ae737cc6
Satanw8you <trusnikolaj41@gmail.com>
2024-04-25 13:21:17 +0300

    edit contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,13 +3,12 @@ import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 
 import css from "./ContactList.module.css";
-import { selectContacts, selectFilteredContacts } from "../../redux/contactsSlice";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
   const filterContacts = useSelector(selectFilteredContacts);
 
-  return <ul>{contacts && filterContacts.map((contact) => <Contact key={contact.id} contact={contact} />)}</ul>;
+  return <ul>{filterContacts.length > 0 && filterContacts.map((contact) => <Contact key={contact.id} contact={contact} />)}</ul>;
 };
 
 export default ContactList;
